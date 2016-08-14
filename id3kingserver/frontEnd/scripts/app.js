0f3ab5ee81d9fb132f9f756cc72ad8833bd23b27
Luxor002 <stefano.belli4@studio.unibo.it>
2016-08-14 18:42:12 +0200

    Finiti la maggior parte dei filtri, manca solo: la ricerca per luogo, il filtraggio per data.

--- id3kingserver/frontEnd/scripts/app.js
+++ id3kingserver/frontEnd/scripts/app.js
@@ -1,17 +1,11 @@
 var app = angular.module('id3king', ['ui.bootstrap']);
 
-app.run(function($templateCache) {
-  $templateCache.put('numberFilter.html', `
-      <input type="number" class="form-control">
-      <span class="measureUnit middleVertical" ng-if="selectedType == 'Dislivello' || selectedType == 'Lunghezza'">
-          <span>{{selectedType == 'Dislivello' ? 'm' : 'Km'}}</span>
-      </span>`);
-  $templateCache.put('hoursFilter.html', `<div class="relative w20"><div class="hoursPicker" uib-timepicker min="0" ng-model="startingHours" hour-step="hours" minute-step="minutes" show-meridian="false">
-      </div>
-      <span class="hourIndicator">H</span>
-      </div>`);
-});
-
+const difficoltaValues = {
+  T: 0,
+  E: 1,
+  EE: 2,
+  EAI: 3,
+}
 app.controller('dataController', ['$scope', '$http', function($scope, $http) {
     $scope.openFiltersMenu = false;
     $scope.orderings = {
@@ -23,6 +17,7 @@ app.controller('dataController', ['$scope', '$http', function($scope, $http) {
             method: 'POST',
             url: '/getData'
         }).then(function successCallback(response) {
+          $scope.difficultyTypes = [];
             $scope.itinerari = response.data.map(function(itinerario) {
                 var values = itinerario.Data.split('/');
                 var newDate = new Date();
@@ -31,6 +26,14 @@ app.controller('dataController', ['$scope', '$http', function($scope, $http) {
                 newDate.setDate(values[0]);
 
                 itinerario.Data = newDate;
+
+                itinerario.Difficolta = {
+                  Difficolta: itinerario.Difficolta,
+                  value: difficoltaValues[itinerario.Difficolta]
+                }
+
+                if($scope.difficultyTypes.indexOf(itinerario.Difficolta.Difficolta) == -1)
+                  $scope.difficultyTypes.push(itinerario.Difficolta.Difficolta);
                 return itinerario;
             });
         });
@@ -77,6 +80,10 @@ app.controller('dataController', ['$scope', '$http', function($scope, $http) {
     $scope.addFilter = function() {
         $scope.filters.push({});
     }
+    $scope.deleteFilter = function(filter){
+      var index = $scope.filters.indexOf(filter);
+      $scope.filters.splice(index, 1);
+    }
 }]);
 
 //thanks http://stackoverflow.com/a/17648547
@@ -97,10 +104,39 @@ app.filter('numberFixedLen', function() {
 
 app.filter('time', ['$filter', function($filter) {
     return function(minutes) {
-        var hours = (minutes / 60).toFixed();
+        var hours = Math.floor(minutes / 60);
         return hours + "h " + minutes % 60 + "m";
     };
 }]);
+
+app.filter('trackFilter', function($rootScope) {
+  return function(items, filters, scope) {
+    if(filters.length == 0)
+      return items;
+
+      var filteredItems = [];
+      items.forEach(function(item) {
+        var passed= true;
+
+        //scorri tutti i filtri che l'utente ha inserito..
+        for(var i=0; i < filters.length; i++) {
+          var filter = filters[i];
+          if(filter.type != undefined && filter.value != undefined) {
+            var operator = filter.operator == '=' ? '==' : filter.operator;
+            var value = typeof item[filter.type] == 'object' ? item[filter.type].value : item[filter.type];
+
+            var expr = value + ' ' + operator + ' ' + filter.value;
+            passed = $rootScope.$eval(expr);
+            if(!passed)
+              break;
+          }
+        }
+        if(passed)
+         filteredItems.push(item);
+      });
+      return filteredItems;
+    }
+});
 
 app.directive('filtersBar', ['$timeout', function($timeout) {
     return {
@@ -121,7 +157,7 @@ app.directive('filtersBar', ['$timeout', function($timeout) {
                         var width = expand ? element.parent()[0].offsetWidth : 0;
                         element.css('width', width + 'px');
                     }, 0);
-                });
+            });
             scope.close = function(){
               scope.expand = false;
             }
@@ -129,47 +165,76 @@ app.directive('filtersBar', ['$timeout', function($timeout) {
     };
 }]);
 
+app.run(function($templateCache) {
+  $templateCache.put('choiceFilter.html', `
+  <div class="btn-group" uib-dropdown dropdown-append-to-body is-open="true">
+      <button id="btn-append-to-body" type="button" class="btn btn-primary" uib-dropdown-toggle>
+        <span ng-if="!difficoltaValues.value">Seleziona..</span>
+        <span ng-if="difficoltaValues.value">{{difficultyValue.value}}</span>
+        <span class="caret middleVertical"></span>
+      </button>
+    <ul class="dropdown-menu" uib-dropdown-menu role="menu" aria-labelledby="btn-append-to-body">
+      <li ng-repeat="difficulty in difficultyTypes" role="menuitem"><a class="click" ng-click="difficultyValue.value = difficulty">{{difficulty}}</a></li>
+    </ul>
+  </div>`);
+  $templateCache.put('numberFilter.html', `
+      <input type="number" class="form-control" ng-model="numberValue.value">
+          <span class="measureUnit middleVertical" ng-if="filter.type == 'Dislivello' || filter.type == 'Lunghezza'">
+          <span>{{filter.type == 'Dislivello' ? 'm' : 'Km'}}</span></span>
+        </input>`);
+  $templateCache.put('hoursFilter.html', `<div class="relative w20"><div class="hoursPicker" uib-timepicker ng-model="hoursValue.value" hour-step="hours" minute-step="minutes" show-meridian="false">
+      </div>
+      <span class="hourIndicator">H</span>
+      </div>`);
+});
+
 app.directive('filterElement', ['$timeout', function($timeout) {
     return {
         restrict: 'E',
-        template: `<div class="filterElement">
+        template: `<div class="filterElement relative">
+              <span class="deleteElement click" ng-click="deleteFunction(filter)">&times;</span>
               <div class="pull-left">
                   <div class="btn-group" uib-dropdown dropdown-append-to-body is-open="true">
                     <button id="btn-append-to-body" type="button" class="btn btn-primary" uib-dropdown-toggle>
-                    <span ng-if="!selectedType">Filtro</span>
-                      <span ng-if="selectedType">{{selectedType}}</span>
+                    <span ng-if="!filter.type">Filtro</span>
+                      <span ng-if="filter.type">{{filter.type}}</span>
                       <span class="caret middleVertical"></span>
                     </button>
                     <ul class="dropdown-menu" uib-dropdown-menu role="menu" aria-labelledby="btn-append-to-body">
                       <li role="menuitem"><a class="click" ng-click="selectType('ID')">ID</a></li>
-                      <li role="menuitem"><a class="click" ng-click="selectType('Data')">Data</a></li>
+                      <li role="menuitem" ng-if="false"><a class="click" ng-click="selectType('Data')">Data</a></li>
                       <li role="menuitem"><a class="click" ng-click="selectType('Durata')">Durata</a></li>
                       <li role="menuitem"><a class="click" ng-click="selectType('Lunghezza')">Lunghezza</a></li>
                       <li role="menuitem"><a class="click" ng-click="selectType('Dislivello')">Dislivello</a></li>
                       <li role="menuitem"><a class="click" ng-click="selectType('Difficolta')">Difficolta</a></li>
-                      <li role="menuitem"><a class="click" ng-click="selectType('Luogo')">Luogo</a></li>
+                      <li role="menuitem" ng-if="false"><a class="click" ng-click="selectType('Luogo')">Luogo</a></li>
                     </ul>
                   </div>
                 </div>
-              <div class="operatorSelector click pull-left relative preserve" ng-if="selectedType" ng-click="changeOperator(currentOperator)">
-                  <span class="middle">{{currentOperator}}</span>
+              <div class="operatorSelector click pull-left relative preserve" ng-if="filter.type" ng-click="changeOperator(filter.operator)">
+                  <span class="middle">{{filter.operator}}</span>
               </div>
-              <div class="inputValue pull-left w25 relative" ng-class="selectedType == 'Durata' ? 'noBorderBottom' : ''" ng-if="selectedType">
-                <div ng-include="getFilterTemplate(selectedType)"> </div>
+              <div class="inputValue pull-left w25 relative" ng-class="filter.type == 'Durata' ? 'noBorderBottom' : ''" ng-if="filter.type">
+                <div ng-include="getFilterTemplate(filter.type)"> </div>
               </div>
           </div>`,
         replace: true,
         scope: {
-            filter: '='
+            filter: '=',
+            deleteFunction: '=',
+            difficultyTypes: '='
         },
         transclude: true,
         link: function(scope, element, attrs) {
-            scope.isopen = false;
-            scope.selectedType = undefined;
-            scope.currentOperator = '>';
+            scope.filter.value = undefined;
+            scope.filter.operator = '>';
+            scope.filter.type = undefined;
 
             scope.selectType = function(category) {
-                scope.selectedType = category;
+                if(category != scope.filter.type)
+                  scope.filter.value = undefined;
+
+                scope.filter.type = category;
             }
             scope.changeOperator = function(currentOperator) {
                 var operator = undefined;
@@ -179,7 +244,7 @@ app.directive('filterElement', ['$timeout', function($timeout) {
                     operator = '<';
                 if (currentOperator == '<')
                     operator = '>';
-                scope.currentOperator = operator;
+                scope.filter.operator = operator;
                 return operator;
             }
 
@@ -191,12 +256,45 @@ app.directive('filterElement', ['$timeout', function($timeout) {
                     return 'numberFilter.html';
                   case 'Durata':
                     return 'hoursFilter.html';
+                  case 'Difficolta':
+                    return 'choiceFilter.html';
                 }
             }
 
-            scope.startingHours = new Date(2000, 1, 1);
+            scope.hoursValue = {
+              value : new Date(200, 1, 1)
+            };
             scope.hours = 1;
             scope.minutes = 10;
+            scope.$watch(function() {
+                    return scope.hoursValue.value;
+                },
+                function(value) {
+                  scope.filter.value = value.getHours() * 60 + value.getMinutes();
+            });
+
+            scope.numberValue = {
+              value: undefined
+            }
+            scope.$watch(function() {
+                    return scope.numberValue.value;
+                },
+                function(value) {
+                  if(scope.filter.type == 'Lunghezza')
+                    scope.filter.value = value * 1000;
+                  else
+                    scope.filter.value = value;
+            });
+
+            scope.difficultyValue = {
+              value : undefined
+            };
+            scope.$watch(function() {
+                    return scope.difficultyValue.value;
+                },
+                function(value) {
+                    scope.filter.value = difficoltaValues[value];
+            });
         }
     };
 }]);
