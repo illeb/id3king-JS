34376755666aa979b0ddb1f0d8e3c043017603ba
Luxor001 <stefano.belli.dev@gmail.com>
2017-05-21 16:18:42 +0200

    Tolta una cartella inutile nella root

--- id3kingserver/routes.js
+++
@@ -1,6 +0,0 @@
-var publicApi = require('./publicApi/public.js')
-
-var routes = [].concat(publicApi);
-
-//pubblica un unico array di routes, per darlo in pasto a index.js
-module.exports = routes;
